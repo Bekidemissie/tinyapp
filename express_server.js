cc7ba23b2fbce8f4f7f86c3b5d86647973735f37
Bekidemissie <bekidemissie16@gmail.com>
2023-07-31 20:49:36 -0400

    create views file and cre ate and modified express_server

--- express_server.js
+++ express_server.js
@@ -31,15 +31,29 @@ app.get("/hello", (req, res) => {
    app.get("/fetch", (req, res) => {
     res.send(`a = ${a}`);
    });
+
    app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
   });
+
   app.get("/hello", (req, res) => {
     const templateVars = { greeting: "Hello World!" };
     res.render("hello_world", templateVars);
   });
+
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
   app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL:"http://www.lighthouselabs.ca" };
     res.render("urls_show", templateVars);
+  })
+  
+  app.use(express.urlencoded({ extended: true }));
+  
+  app.post("/urls", (req, res) => {
+    console.log(req.body); // Log the POST request body to the console
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
   });
