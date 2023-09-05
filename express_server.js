b9934f1445520dfa395199730be39a86b5a9ec30
Bekidemissie <bekidemissie16@gmail.com>
2023-09-05 12:20:48 -0400

    dry my code

--- express_server.js
+++ express_server.js
@@ -62,18 +62,6 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
-
 app.get("/urls", (req, res) => {
   const userId = req.cookies['user_id'];
   const templateVars = { urls: urlDatabase };
